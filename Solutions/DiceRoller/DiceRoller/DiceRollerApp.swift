7c80d457fe2e71f88497826d2b679fb17a44892b
chadhindsight <chadrickJ8@gmail.com>
2026-03-19 14:46:26 -0400

    Add the solution for Dice Roller activity

---
+++ Solutions/DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by chadhindsight on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
